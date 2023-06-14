3a158196060f2d60bd6dd54a66b6ff282fb9bcb3
flaviaolivera <126910577+flaviaolivera@users.noreply.github.com>
2023-06-14 15:09:32 +0000

    excusas

--- src/app.js
+++ src/app.js
@@ -6,6 +6,23 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let item1 = who[Math.floor(Math.random() * who.length)];
+  let item2 = action[Math.floor(Math.random() * action.length)];
+  let item3 = what[Math.floor(Math.random() * what.length)];
+  let item4 = when[Math.floor(Math.random() * when.length)];
+  let excuse = item1 + " " + item2 + " " + item3 + " " + item4 + ".";
+
+  document.querySelector("#greeting").innerHTML = excuse;
+  console.log("Hello World");
 };
